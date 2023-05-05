e4dde8248bdcb44057936d1461eb4e5494f2dff5
sanidhya0612 <102963033+sanidhya0612@users.noreply.github.com>
2023-05-05 18:12:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+prediction = "";
+
 Webcam.set({
     width:350,
     height:300,
@@ -10,7 +12,7 @@ camera = document.getElementById("camera");
 Webcam.attach('#camera');
 
       
-function take_snapshot()
+function take_Snapshot()
 {
     Webcam.snap(function(data_uri) {
         document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
@@ -24,3 +26,44 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 function modelLoaded() {
     console.log('Model Loaded!');
   }
+
+function check()
+{
+  img = document.getElementById("captured_image");
+  classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) 
+{
+  if (error) 
+  {
+    console.error(error);
+  } 
+  
+  else 
+  {
+    console.log(results);
+    document.getElementById("result_gesture_name").innerHTML = results[0].label;
+    prediction = results[0].label;
+    speak();
+    if(results[0].label == "tiktok")
+    {
+	    document.getElementById("update_gesture").innerHTML = "&#128076;";
+    }
+    if(results[0].label == "thums up")
+    {
+	    document.getElementById("update_gesture").innerHTML = "&#128077;";
+    }
+    if(results[0].label == "victory")
+    {
+	    document.getElementById("update_gesture").innerHTML = "&#9996;";
+    }
+  }
+}
+
+function speak(){
+  var synth = window.speechSynthesis;
+  speak_data = "The prediction is " + prediction;
+  var utterThis = new SpeechSynthesisUtterance(speak_data);
+  synth.speak(utterThis);
+}
